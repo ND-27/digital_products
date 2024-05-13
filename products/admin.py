c980ddeb0a46f580df647a92075ffa7fcc43c733
ND-27 <navid81vafa@gmail.com>
2024-05-13 15:29:12 +0330

    Add 'filter_horizontal' for ProductAdmin.

--- products/admin.py
+++ products/admin.py
@@ -21,4 +21,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'is_enable', 'created_time']
     list_filter = ['is_enable']
     search_fields = ['title']
+    filter_horizontal = ['categories']
     inlines = [FileInlineAdmin]
