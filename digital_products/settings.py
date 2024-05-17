26fb9cdd21d3f805dd32c18984dbd07112760379
ND-27 <navid81vafa@gmail.com>
2024-05-17 23:32:48 +0330

    Create 'users' app and .

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +108,6 @@ MEDIA_URL = '/media/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authorization
+AUTH_USER_MODEL = 'users.User'
